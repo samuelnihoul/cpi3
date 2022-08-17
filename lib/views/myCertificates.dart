45583b97db767d428bc69a645e3a010ec31c0019
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-08-17 19:50:50 +0200

    uploader

--- lib/views/myCertificates.dart
+++ lib/views/myCertificates.dart
@@ -1,12 +1,38 @@
+
+
 import '../components/appBar.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:typed_data';
 
 class Uploader extends StatelessWidget {
   const Uploader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: ()=>print('hello'), child: Text('Upload'));
+    FilePickerResult? file;
+    return ElevatedButton(
+        onPressed: () async {
+          file = await FilePicker.platform.pickFiles();
+         
+
+if (file != null) {
+  Uint8List? fileBytes = file!.files.first.bytes;
+  String fileName = file!.files.first.name;
+  
+  // Upload file
+  TaskSnapshot task=await FirebaseStorage.instance.ref().child('$fileName').putData(fileBytes??Uint8List(0));
+  print(task.bytesTransferred);
+}
+else {
+  print('No file selected');
+}
+  
+
+        },
+        child: Text('Upload'),
+        /* style: ButtonStyle(shape: MaterialStateProperty()), */);
   }
 }
 
@@ -18,7 +44,8 @@ class MyCertificates extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            const Text('🌞 My Certificates ✨'),
+            
+            const Text('🌞 My Certificates ✨'),Uploader(),
             ListView(
               shrinkWrap: true,
               children: <Widget>[
