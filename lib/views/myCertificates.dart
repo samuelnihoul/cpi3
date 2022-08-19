25712db2997f9d8c2470c012e45f86d1708851dd
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-08-19 23:59:28 +0200

    success

--- lib/views/myCertificates.dart
+++ lib/views/myCertificates.dart
@@ -27,8 +27,9 @@ class Uploader extends StatelessWidget {
               .child('/${user?.displayName}/$fileName')
               .putData(fileBytes ?? Uint8List(0));
           print(task.bytesTransferred);
+          SnackBar(content: Text('success'));
         } else {
-          print('No file selected');
+          SnackBar(content: Text('no file selected'));
         }
       },
       child: Text(
